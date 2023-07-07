b86a92d0d00cd863de19f69aa36f27ba493ed2ba
Barbra <joshturbessi@gmail.com>
2023-07-07 16:26:49 -0500

    34343

--- Cat-Burglar/Assets/_Scripts/CatController.cs
+++ Cat-Burglar/Assets/_Scripts/CatController.cs
@@ -1,60 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
 
 public class CatController : MonoBehaviour
 {
-    public float moveSpeed = 5f;        // Movement speed
-    public float sprintSpeed = 10f;     // Sprint speed
-    public float jumpForce = 5f;        // Jump force
-    public float rotationSpeed = 10f;   // Rotation speed
+    public float walkingSpeed = 7.5f;
+    public float runningSpeed = 11.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Camera playerCamera;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
 
-    private float cameraFollowSpeed = 0.1f;  // Camera follow speed
-    private Vector3 cameraOffset;       // Offset between player and camera
-    private bool isJumping = false;     // Flag to check if the player is jumping
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
 
-    private CharacterController controller;
-    private Transform cameraTransform;
+    [HideInInspector]
+    public bool canMove = true;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
-        cameraTransform = Camera.main.transform;
-        cameraOffset = transform.position - cameraTransform.position;
+        characterController = GetComponent<CharacterController>();
+
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
-        // Player movement
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        bool sprint = Input.GetKey(KeyCode.LeftShift);
+        // We are grounded, so recalculate move direction based on axes
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-        Vector3 movement = new Vector3(horizontal, 0f, vertical).normalized;
-
-        // Rotate player towards movement direction
-        if (movement != Vector3.zero)
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
-            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
-            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            moveDirection.y = jumpSpeed;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
         }
 
-        // Calculate movement speed
-        float speed = sprint ? sprintSpeed : moveSpeed;
-
-        // Apply movement to the player
-        Vector3 moveDirection = transform.TransformDirection(movement);
-        moveDirection *= speed;
-
-        controller.Move(moveDirection);
-
-        // Jumping
-        if (Input.GetButtonDown("Jump") && !controller.isGrounded)
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        if (!characterController.isGrounded)
         {
-            moveDirection.y = jumpForce;
-            isJumping = true;
+            moveDirection.y -= gravity * Time.deltaTime;
         }
 
-        // Camera follow
-        Vector3 targetCameraPosition = transform.position - cameraOffset;
-        cameraTransform.position = Vector3.Lerp(cameraTransform.position, targetCameraPosition, cameraFollowSpeed);
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        // Player and Camera rotation
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
     }
 }
